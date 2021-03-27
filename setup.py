7053dfeca28b9ef35297f0bfa1a607900fd25283
ichbin <alireza.gerami4030@gmail.com>
2021-03-27 15:20:49 +0430

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='gdrive',
+    version='1.0.0',
+    packages=['gdrive'],
+    url='',
+    download_url='https://github.com/Alireza-Gerami/gdrive/archive/main.tar.gz',
+    install_requires=[
+        'PyDrive~=1.3.1'
+    ],
+    license='',
+    author='ichbin',
+    author_email='alireza.gerami4030@gmail.com',
+    description='A tool for work with google drive'
+)
